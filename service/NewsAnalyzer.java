822b275bc52c2f1d2d92cb11347e5a9e693e0afc
lucpc <lucpc@gmai.com>
2026-05-13 11:08:14 -0300

    add Classification enum e NewsAnalyzer para a lógica de classifição de notícias

---
+++ service/NewsAnalyzer.java
@@ -0,0 +1,19 @@
+package service;
+
+public class NewsAnalyzer {
+    public Classification analyze(String text) {
+        int score = 0;
+
+        if (!text.contains("FONTE")) score++;
+        if (text.contains("!!!")) score++;
+        if (text.contains("URGENTE")) score++;
+        if (text.length() < 10) score++;
+
+        if (score == 0) {
+            return Classification.CONFIAVEL;
+        } else if (score == 1) {
+            return Classification.DUVIDOSA;
+        }
+        return Classification.FALSA;
+    }
+}
